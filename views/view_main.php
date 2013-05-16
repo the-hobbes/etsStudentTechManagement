bec68d1b81b13a67ec2e6964d0e4077234c59ac7
the-hobbes <phelan.vendeville@gmail.com>
2013-05-16 15:42:08 -0400

    working on layout

--- views/view_main.php
+++ views/view_main.php
@@ -1,13 +1,24 @@
+		<script> 
+			// testing data
+			testData = { "row1": {"info": "some info"},
+							"row2": {"info": "meee!"},
+							"row3": {"info": "some info"},
+							"row4": {"info": "some info"},
+							"row5": {"info": "some info"}
+			};
+			// console.log(testData['row2']);
+		</script>
 		<div id='content' class='row-fluid'>
-			<div class='span9 main'>
+			<div class='span10 main'>
 				<h2>Main Content Section</h2>
 
+				<!-- results from query go here -->
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.<p> 
 				<p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
 
 			</div> <!-- end title -->
 
-			<div class='span3 sidebar'>
+			<div class='span2 sidebar'>
 				<h2>Sidebar</h2>
 
 				 <ul class="nav nav-tabs nav-stacked">
@@ -17,5 +28,5 @@
 				</ul> <!-- end sidebar nav -->
 
 			</div> <!-- end sidebar -->
-			
+
 		</div> <!-- end content -->
