b7649a61f45aa5ddd0debedf0c93d266c216aea4
the-hobbes <phelan.vendeville@gmail.com>
2013-05-21 14:35:16 -0400

    added table sorting and styling capability to people, payroll and appliation tables

--- views/view_application.php
+++ views/view_application.php
@@ -5,35 +5,44 @@ $applications = $data['applications'];
 <h2>ETS Applications</h2>
 
 <div id='content' class='row-fluid'>
-	<table class="table table-striped">
-		<tr>
-			<th>Netid</th>
-			<th>First Name</th>
-			<th>Last Name</th>
-			<th>Submission Date</th>
-			<th>Graduation Date</th>
-		</tr>
+	<table id="applicationsTable" class="table table-striped">
+		<thead>
+			<tr>
+				<th>Netid</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Submission Date</th>
+				<th>Graduation Date</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			foreach($applications as $application){
+				$html = "<tr>
+								<td><a href=\"".siteURL('people/profile/'.$application['pk_netid'])."#application\">".$application['pk_netid']."</a></td>
+								<td>".$application['fld_firstname']."</td>
+								<td>".$application['fld_lastname']."</td>
+								<td>".$application['fld_submissiondate']."</td>
+								<td>".$application['fld_graddate']."</td>
+						</tr>";
+				echo $html;
 
-		<?php
-		foreach($applications as $application){
-			$html = "<tr>
-							<td><a href=\"".siteURL('people/profile/'.$application['pk_netid'])."#application\">".$application['pk_netid']."</a></td>
-							<td>".$application['fld_firstname']."</td>
-							<td>".$application['fld_lastname']."</td>
-							<td>".$application['fld_submissiondate']."</td>
-							<td>".$application['fld_graddate']."</td>
-					</tr>";
-			echo $html;
+			}
 
-		}
-
-		?>
-
-
+			?>
+		</tbody>
 	</table>
 
 </div><!--end content-->
+<script type="text/javascript">
+	// sortable tables
+	$(document).ready(function() 
+	    { 
+	        $("#applicationsTable").tablesorter(); 
+	    } 
+	); 
 
+</script>
 <?php
 
 	
--- views/view_people.php
+++ views/view_people.php
@@ -4,38 +4,51 @@
 		<h2>ETS Employees</h2>
 
 		<!-- results from query go here -->
-		<table class="table table-striped">
-			<tr>
-				<th>Netid</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th>Middle Initial</th>
-				<th>Email</th>
-				<th>Sched. Code</th>
-				<th>Major</th>
-				<th>Grad Date</th>
-				<th>Phone</th>
-			</tr>
-		<?php
-			foreach($data['people'] as $person){
-				$html="
-					<tr>
-						<td><a href=\"".siteURL('people/profile/'.$person['pk_netid'])."#general\">".$person['pk_netid']."</td>
-						<td>".$person['fld_firstname']."</td>
-						<td>".$person['fld_lastname']."</td>
-						<td>".$person['fld_middleinitial']."</td>
-						<td>".$person['fld_email']."</td>
-						<td>".$person['fld_schedulecode']."</td>
-						<td>".$person['fld_major']."</td>
-						<td>".$person['fld_graddate']."</td>
-						<td>".$person['fld_phone']."</td>
-					</tr>
-				";
-				echo $html;
-			}
-		?>
+		<table id="employeeTable"class="table table-striped tablesorter">
+			<thead>
+				<tr>
+					<th>Netid</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Middle Initial</th>
+					<th>Email</th>
+					<th>Sched. Code</th>
+					<th>Major</th>
+					<th>Grad Date</th>
+					<th>Phone</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					foreach($data['people'] as $person){
+						$html="
+							<tr>
+								<td><a href=\"".siteURL('people/profile/'.$person['pk_netid'])."#general\">".$person['pk_netid']."</td>
+								<td>".$person['fld_firstname']."</td>
+								<td>".$person['fld_lastname']."</td>
+								<td>".$person['fld_middleinitial']."</td>
+								<td>".$person['fld_email']."</td>
+								<td>".$person['fld_schedulecode']."</td>
+								<td>".$person['fld_major']."</td>
+								<td>".$person['fld_graddate']."</td>
+								<td>".$person['fld_phone']."</td>
+							</tr>
+						";
+						echo $html;
+					}
+				?>
+			</tbody>
 		</table>
 
 <!-- http://www.appelsiini.net/projects/jeditable -->
 
 </div> <!-- end content -->
+<script type="text/javascript">
+	// sortable tables
+	$(document).ready(function() 
+	    { 
+	        $("#employeeTable").tablesorter(); 
+	    } 
+	); 
+
+</script>
