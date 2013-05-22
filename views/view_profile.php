4be23ddc699acd73ad6f04970f24515379632634
Michael Toth <mtotho@gmail.com>
2013-05-22 14:52:53 -0400

    added hire/deny button. Hire button works. adds nearly empty payroll table. made budget code, rec num, etc editable

--- views/view_profile.php
+++ views/view_profile.php
@@ -158,17 +158,17 @@
 
 				<tr>
 					<td>Employee ID</td>
-					<td><?php echo $payroll['fld_employeeid'] ?></td>
+					<td id="<?php echo $person['fld_hashkey'].".fld_employeeid.tbl_payroll" ?>" class="edit"><?php echo $payroll['fld_employeeid'] ?></td>
 				</tr>
 
 				<tr>
 					<td>Budget Code</td>
-					<td><?php echo $payroll['fld_budgetcode'] ?></td>
+					<td id="<?php echo $person['fld_hashkey'].".fld_budgetcode.tbl_payroll" ?>" class="edit"><?php echo $payroll['fld_budgetcode'] ?></td>
 				</tr>
 
 				<tr>
 					<td>Employee rec num</td>
-					<td><?php echo $payroll['fld_employeerecnum'] ?></td>
+					<td id="<?php echo $person['fld_hashkey'].".fld_employeerecnum.tbl_payroll" ?>" class="edit"><?php echo $payroll['fld_employeerecnum'] ?></td>
 				</tr>
 
 				<tr>
